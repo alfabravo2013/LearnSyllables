3ebcd733310ffa176a486f9b7b5b8c888536e118
Viktor Martynov <2013alfabravo@gmail.com>
2021-05-03 03:30:00 +0400

    change TTS error messages to snackbars

--- app/src/main/java/com/github/alfabravo2013/learnsyllables/MainActivity.kt
+++ app/src/main/java/com/github/alfabravo2013/learnsyllables/MainActivity.kt
@@ -3,12 +3,11 @@ package com.github.alfabravo2013.learnsyllables
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.speech.tts.Voice
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
@@ -25,7 +24,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         setContentView(R.layout.activity_main)
 
         val consonantsAdapter = ConsonantsAdapter(this, viewModel)
-        val syllablesAdapter = SyllablesAdapter(this) { speakOut(it) }
+        val syllablesAdapter = SyllablesAdapter(this) { text -> speakOut(text) }
 
         rvSyllables = findViewById(R.id.rvSyllables)
         rvConsonants = findViewById(R.id.rvConsonants)
@@ -52,15 +51,19 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             val result = tts.setLanguage(Locale("ru"))
 
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS", "The Language specified is not supported!")
+                Snackbar
+                    .make(rvSyllables, "Язык не поддерживается", Snackbar.LENGTH_SHORT)
+                    .show()
             }
         } else {
-            Log.e("TTS", "Initialization failed!")
+            Snackbar
+                .make(rvSyllables, "Не могу подключить Text-To-Speech", Snackbar.LENGTH_SHORT)
+                .show()
         }
     }
 
     private fun speakOut(text: CharSequence) {
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+        tts.speak(text, TextToSpeech.QUEUE_ADD, null, "")
     }
 
     override fun onDestroy() {
